8e84ffed0502f5c23fc9ad79e23596ca2dd3ae15
Aryel <126723967+aryelnascimento11@users.noreply.github.com>
2023-10-03 16:36:13 -0300

    criando do zero

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,29 @@
-export default function Header() {
-    return (
+"use client"
 
-)
+import { describe } from "node:test"
+import { useState } from "react"
+
+export default function Header() {
+
+    const [text, setText] = useState("blue")
+    const [list, setList] = useState("id, title")
+
+    function trocar() {
+        setText("red")
+        console.log(text)
+    }
+
+
+    return (
+        <>
+        <header className="bg-black top-0  w-screen   p-6  shadow-lg rounded-bl-lg rounded-br-lg '>">
+            <h1 
+             style={{color:text} }
+            className="text-3xl text-center justify-center flex text-white">{text}</h1>
+            <button 
+           
+            onClick={trocar} className="text-3xl text-white">ooooo</button>
+            </header>
+        </>
+    )
 }
